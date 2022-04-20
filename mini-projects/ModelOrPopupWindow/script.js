b3c1253983c33e66125c87b88caebad02f53b9ed
ruipinto <ruifspinto@gmail.com>
2022-04-20 07:28:38 +0100

    incomplete modal example

---
+++ mini-projects/ModelOrPopupWindow/script.js
@@ -0,0 +1,13 @@
+let modalContent = document.querySelector(".modal-content");
+let openModal = document.querySelector(".open-modal");
+let closeModal = document.querySelector(".close-modal");
+let blurBg = document.querySelector(".blur-bg");
+
+openModal.addEventListener("click", function () {
+  modalContent.classList.remove("hidden-modal");
+  blurBg.classList.remove("hidden-blur");
+});
+
+closeModal.addEventListener("click", function() {
+  modalContent.getElementsByClassName.display = "none";
+})
