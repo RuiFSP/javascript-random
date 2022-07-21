5aeed969f6e30b37b9af4b2b58dfe2de289102a9
ruipinto <ruifspinto@gmail.com>
2022-07-21 18:53:21 +0100

    exercises about functions returning functions, call,apply,bind methods, partial application , coding challenge1 from udemy

--- udemyJonasJS/examples/functions/functions.js
+++ udemyJonasJS/examples/functions/functions.js
@@ -138,7 +138,7 @@ function count() {
 //higher order-function exist */
 
 //-------------------------------------------------------------------------------------------
-//---------------------- FUNCTIONS ACCEPTING CALL BACK FUNCTIONS ----------------------------
+//---------------------4) FUNCTIONS ACCEPTING CALL BACK FUNCTIONS ---------------------------
 //-------------------------------------------------------------------------------------------
 
 /* //create a string without any spaces
@@ -173,5 +173,156 @@ document.body.addEventListener('click', high5);
 ['rui', 'pedro', 'ana'].forEach(high5); //3 wavings */
 
 //-------------------------------------------------------------------------------------------
-//--------------------------- FUNCTIONS RETURNING FUNCTIONS ---------------------------------
+//-------------------------5) FUNCTIONS RETURNING FUNCTIONS ---------------------------------
 //-------------------------------------------------------------------------------------------
+
+/* const greet = greeting => {
+  return name => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey'); //this value i actually a function
+
+greeterHey('Jonas'); //this works because greeterHey is a function //Hey Jonas
+greeterHey('Steven'); //Hey Steven
+
+//this work because of something called "CLOSURE"
+
+greet('Hello')('Jonas'); //this is weird but works, because (greet('Hello')) is a function and it´s argument will be ('Jonas')
+
+//what is the point of having functions returning functions  ?? ---> this is usefull for functional programming
+
+//challenge simplify arrow function
+
+const greet1 = greeting1 => name => console.log(`${greeting1} ${name}`);
+greet1('Welcome')('Rui'); */
+
+//-------------------------------------------------------------------------------------------
+//--------------------------- 6) The CALL, APPLY, BIND Methods ------------------------------
+//-------------------------------------------------------------------------------------------
+
+const lufthansa = {
+  airline: ' Luthansa',
+  iataCode: 'LM',
+  bookings: [],
+
+  //book: function(){}
+  //enhanced function
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name }); //pushing entries to array
+  },
+};
+
+lufthansa.book(239, 'Rui Pinto'); //adding 1 booking
+lufthansa.book(635, 'Taina Sousa'); //adding second booking
+console.log(lufthansa); //booking has 2 values
+
+const eurowings = {
+  airline: 'EuroWings',
+  iataCode: 'EW',
+  bookings: [],
+
+  //bad practice to copy the function - extra boilerplate - check bellow
+};
+
+//we should creat a new function to store the method
+const book = lufthansa.book;
+
+//book(23, 'Sarah Williams'); //this boook function is no longer the method inside lufthansa, the this is pointing to undefined - this does not work
+//we need to tell JS , what this keyword is pointing ? 3 methods can be used call(), apply() and bind()
+
+//--------- CALL METHOD --------
+book.call(eurowings, 23, 'Sarah Williams'); //we did not call the book method ourself, instead we called the call() method. It will call the book method, with this keyword of eurowings.
+//this allow us the manually call a method -
+console.log(eurowings); //Sarah Williams booked a seat on undefined flight EW23
+
+book.call(lufthansa, 239, 'Pedro Simão'); //bookings now have 3 values
+console.log(lufthansa); //Pedro Simão booked a seat on  Luthansa flight LM239
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Pedro Simão');
+console.log(swiss);
+
+//--------- APPLY METHOD --------
+//similar to call method but it does not recieve a list of arguments like call arg(583, 'Pedro Simão'), but an array
+
+const flightData = [538, 'Rui Pinto'];
+
+book.apply(swiss, flightData); //recives an array of data
+console.log(swiss);
+//it is not that used in modern Javascript
+
+book.call(swiss, ...flightData); //it is the same has above and just use spread
+// book.apply(swiss, flightData) = book.call(swiss, ...flightData) .. take the last approach
+//---> this allow us to used explicity the "this" keyword in any function
+
+//--------- BIND METHOD --------
+//probably more important than call and apply
+//The big difference is that bond does not immediatelly calls the function, instead return a new function where the "this" keyword is bound
+
+const bookEW = book.bind(eurowings); //now the keyword is bound to eurowings
+
+bookEW(23, 'Steven Williams'); //we don´t need to specify the this keyword now
+console.log(eurowings); //it is EuroWings booking array
+
+//This makes it easier to book fights if we create abind for each one. let´s create for the other companies
+const bookLH = book.bind(lufthansa); //now the keyword is bound to lufthansa
+const bookLX = book.bind(swiss); //now the keyword is bound to swiss
+
+//but we can pass also arguments for specific stuff
+
+const bookEW23 = book.bind(eurowings, 23); //for this case 23 will be the defaulf and the function only needs the name
+
+bookEW23('pedro paupa');
+bookEW23('joão xpto');
+console.log(eurowings); //4 bookings , with the last two values added
+
+//this a pattern called PARTIAL APPLICATION
+
+// ---- > Another application is for objects with event listeners
+lufthansa.planes = 300; //new property
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+//document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane); //NaN , this is pointing for button, this is why we get NaN
+
+//solution
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); //this keyword is bound to the luftansa object
+
+//----> Another example for bind method - partial application (pre-set parameters). In this case we don´t even care about the this keyword
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200)); //2200
+
+const addVAT = addTax.bind(null, 0.23); //we don´t care about the this keyword, so we use null and set the tax rate to 23%
+// same as addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100)); //123
+console.log(addVAT(23)); //28.29
+
+//Challenge use the technique "function returning a function" for above example instead of bind
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+//same results as above //123 and 28.29
